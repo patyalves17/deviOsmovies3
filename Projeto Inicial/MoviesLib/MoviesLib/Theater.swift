1d9e8834c4e497278e917ad05c9381e2d2d5b09d
Usuário Convidado <Guest@PA06MICRO14.local>
2017-09-06 21:22:27 -0300

    maps e xml

---
+++ Projeto Inicial/MoviesLib/MoviesLib/Theater.swift
@@ -0,0 +1,23 @@
+//
+//  Theater.swift
+//  MoviesLib
+//
+//  Created by Usuário Convidado on 06/09/17.
+//  Copyright © 2017 EricBrito. All rights reserved.
+//
+
+import Foundation
+
+struct Theater{
+    var name: String = ""
+    var endereco: String = ""
+    var latitude: Double = 0.0
+    var longitude: Double = 0.0
+    var url: String = ""
+    var address: String = ""
+    
+    init(){
+    
+    }
+    
+}
--- Projeto Inicial/MoviesLib/MoviesLib/TheaterMapViewController.swift
+++ Projeto Inicial/MoviesLib/MoviesLib/TheaterMapViewController.swift
@@ -7,20 +7,50 @@
 //
 
 import UIKit
+import MapKit
 
 class TheaterMapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    var currentElement: String!
+    var theater: Theater!
+    var theaters: [Theater] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        loadXML()
+        print(theaters.count)
     }
+    
+    func addTheaters(){
+        for theater in theaters{
+            let coordinate = CLLocationCoordinate2D(latitude: theater.latitude, longitude: theater.longitude)
+            let annotation = TheaterAnnotation(coordinate: coordinate)
+            
+            annotation.title=theater.name
+            annotation.subtitle=theater.address
+            
+            mapView.addAnnotation(annotation)
+        }
+        
+        mapView.showAnnotations(mapView.annotations, animated: true)
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    func loadXML(){
+        if let xmlURL = Bundle.main.url(forResource: "theaters", withExtension: "xml"),
+            let xmlParser = XMLParser(contentsOf: xmlURL){
+            xmlParser.delegate = self
+            xmlParser.parse()
+        }
+    }
+
+   
 
     /*
     // MARK: - Navigation
@@ -32,4 +62,52 @@ class TheaterMapViewController: UIViewController {
     }
     */
 
+}
+
+extension TheaterMapViewController: XMLParserDelegate{
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        //print("Conteudo ",elementName)
+        currentElement = elementName
+        if elementName == "Theater" {
+            theater = Theater()
+            
+        }
+    }
+    
+ 
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        print("Conteudo: ", string)
+        
+        let content=string.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        if !content.isEmpty {
+            switch currentElement {
+            case "name":
+                theater.name = content
+            case "address":
+                theater.address = content
+            case "latitude":
+                theater.latitude = Double(content)!
+            case "longitude":
+                theater.longitude = Double(content)!
+            case "url":
+                theater.url = content
+            default:
+                break
+            }
+        }
+        
+    }
+    
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        print("Fim: ",elementName)
+        if elementName == "Theater"{
+            theaters.append(theater)
+            
+        }
+    }
+    
+    func parserDidEndDocument(_ parser: XMLParser) {
+        addTheaters()
+    }
+    
 }
